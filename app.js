9357eddfdb5e7b7a926b5e7b4703d740cffa1739
vjdbarayuga <vjdbarayuga@gmail.com>
2024-09-10 07:26:40 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+// //AND
+
+// let x = 10;
+// let y = 20;
+// let z = 30;
+
+// console.log(x < y && y < z); //(x is less than y and y is less than z)
+// console.log(x > y && y > z); // (both of the conditions are false)
+
+//OR
+let a = 'hello';
+let b = 'world';
+let c = 'world';
+
+console.log(a === b || a === c); // (neither condition is true)
+console.log(a === b || b === c); // (true, one condition is true)
+
+//NOT
+let isSunny = true;
+let isRainy = false;
+
+// console.log(!isSunny);
+// console.log(!isRainy);
+
+//console.log(!(isSunny && isRainy));
+console.log(isSunny || isRainy);
